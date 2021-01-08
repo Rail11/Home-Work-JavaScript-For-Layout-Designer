8e21ed83ad9ace0c4f18042bba06194900260ee7
Rail <polzovatel_polzovatel_1910@mail.ru>
2021-01-08 14:22:35 +0500

    lesson_1 main task complete

---
+++ lesson_1/1_main_task/script.js
@@ -0,0 +1,17 @@
+'use strict';
+let budget = prompt(`Ваш бюджет?`),
+    shopName = prompt(`Название вашего магазина?`);
+
+let mainList = {
+    budget,
+    shopName,
+    shopGoods: [],
+    employers,
+    open
+}
+
+shopGoods[0] = prompt(`Какой тип товаров будем продавать?`);
+shopGoods[1] = prompt(`Какой тип товаров будем продавать?`);
+shopGoods[2] = prompt(`Какой тип товаров будем продавать?`);
+
+alert(budget / 30);
