5502bd02e07b1db1d4a11164686993e3eb94ad86
Rail <polzovatel_polzovatel_1910@mail.ru>
2021-02-23 20:30:57 +0500

    lesson_12 main task complete

--- lesson_11 Работа с JSON, AJAX. Отправка данных на сервер/1_main_task/js/script.js
+++ lesson_11 Работа с JSON, AJAX. Отправка данных на сервер/1_main_task/js/script.js
@@ -114,22 +114,50 @@ window.addEventListener('DOMContentLoaded', function() {
     showAndHideModal(`.more`);
     showAndHideModal(`.description-btn`);
 
-    /* ПЛАВНАЯ ПРОКРУТКА */
-
-    const anchors = document.querySelectorAll(`.scroll-to`);
-
+    /* Плавная прокрутка страницы по якорям (по ссылкам) */
+    /* Способ №1 */
+    /* const anchors = document.querySelectorAll(`.scroll-to`);
     for (let item of anchors) {
-        item.addEventListener(`click`, function (e) {
+        item.addEventListener('click', function (e) {
           e.preventDefault()
           
-          const blockID = item.getAttribute(`href`)
-          /* scrollIntoView() делает прокрутку в зависимости от параметров behavior и block */
+          const blockID = item.getAttribute('href')
+        //   scrollIntoView() делает прокрутку в замисимости от параметров behavior и block
           document.querySelector(blockID).scrollIntoView({
-            behavior: `smooth`,
-            block: `start`
+            behavior: 'smooth',
+            block: 'start'
           })
         })
+    } */
+    /* Способ №2 */
+    function animate(draw, duration) {
+        let start = performance.now();
+        requestAnimationFrame(
+            function animate(time) {
+                let timePassed = time - start;
+                if (timePassed > duration) {
+                    timePassed = duration;
+                }
+
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+                }
+            }
+        )
     }
+
+    let menu = document.getElementsByTagName('nav')[0];
+
+    menu.addEventListener('click', function(event) {
+        event.preventDefault();
+        animate(function(timePassed) {
+            let target = event.target;
+            let section = document.getElementById(target.getAttribute('href').slice(1));
+            window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+        }, 1500)
+    })
 
     /* FORM */
     /* Работает на элементах c тегом form */
@@ -190,4 +218,4 @@ window.addEventListener('DOMContentLoaded', function() {
     sendingData('.main-form');
     sendingData('#form');
 
-})
+});
