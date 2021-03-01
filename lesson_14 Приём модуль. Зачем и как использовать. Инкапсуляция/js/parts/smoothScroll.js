f344ee9084d4108adc33e4aaa9522d72a73c3036
Rail <polzovatel_polzovatel_1910@mail.ru>
2021-03-01 10:30:26 +0500

    lesson_14 used browserify

---
+++ lesson_14 Приём модуль. Зачем и как использовать. Инкапсуляция/js/parts/smoothScroll.js
@@ -0,0 +1,48 @@
+function smoothScroll() {
+    /* Плавная прокрутка страницы по якорям (по ссылкам) */
+    /* Способ №1 */
+    /* const anchors = document.querySelectorAll(`.scroll-to`);
+    for (let item of anchors) {
+        item.addEventListener('click', function (e) {
+          e.preventDefault()
+          
+          const blockID = item.getAttribute('href')
+        //   scrollIntoView() делает прокрутку в замисимости от параметров behavior и block
+          document.querySelector(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          })
+        })
+    } */
+    /* Способ №2 */
+    function animate(draw, duration) {
+        let start = performance.now();
+        requestAnimationFrame(
+            function animate(time) {
+                let timePassed = time - start;
+                if (timePassed > duration) {
+                    timePassed = duration;
+                };
+
+                draw(timePassed);
+
+                if (timePassed < duration) {
+                    requestAnimationFrame(animate);
+                };
+            }
+        );
+    };
+
+    let menu = document.getElementsByTagName('nav')[0];
+
+    menu.addEventListener('click', function(event) {
+        event.preventDefault();
+        animate(function(timePassed) {
+            let target = event.target;
+            let section = document.getElementById(target.getAttribute('href').slice(1));
+            window.scrollBy(0, section.getBoundingClientRect().top / 20 - 3);
+        }, 1500)
+    });
+};
+
+module.exports = smoothScroll;
